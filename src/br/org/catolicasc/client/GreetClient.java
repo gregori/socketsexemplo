beda3860fe3724eff0fbea0d93ce2d410f525846
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2023-06-06 21:47:04 -0300

    Preencher métodos start, stop e sendMessage

--- src/br/org/catolicasc/client/GreetClient.java
+++ src/br/org/catolicasc/client/GreetClient.java
@@ -1,6 +1,8 @@
 package br.org.catolicasc.client;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -9,15 +11,30 @@ public class GreetClient {
     private PrintWriter out;
     private BufferedReader in;
 
-    public void start(String ip, int port) {
-
+    public void start(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        // handler para escrita de dados
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        // handler lara leitura de dados
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
 
     public void stop() {
-
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Erro ao fechar a conexão.");
+        }
     }
 
-    public String sendMessage(String msg) {
-        return null;
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);  // manda a msg para o socket
+        return in.readLine();  // retorna a mensagem recebida do socket
+    }
+
+    public static void main(String[] args) {
+
     }
 }
